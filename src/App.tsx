7c5eec5a52202c96649fc916a555fd531ceb7f74
insookim0702 <insookim0702@gmail.com>
2021-06-02 22:27:25 +0900

    typescript + react + prettierrc 환경 세팅

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>노션 개발하기</h1>
+        </div>
+    );
+}
+
+export default App;
